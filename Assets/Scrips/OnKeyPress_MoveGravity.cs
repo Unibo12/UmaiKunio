8f414cb8f1531a84b9b8cdff3f1c3a4c7cf8f29c
github Unibo12 <ryosukeknzw@gmail.com>
2019-04-16 01:03:24 +0900

    移動処理の無駄な記述を削除

--- Assets/Scrips/OnKeyPress_MoveGravity.cs
+++ Assets/Scrips/OnKeyPress_MoveGravity.cs
@@ -18,6 +18,7 @@ public class OnKeyPress_MoveGravity : MonoBehaviour {
 	
 	void Update () { // ずっと行う
 		vx = 0;
+        pos = transform.position;
 
         // もし、右キーが押されたら
         if (Input.GetKey("right") || Input.GetAxis("Horizontal") > 0)
@@ -25,9 +26,7 @@ public class OnKeyPress_MoveGravity : MonoBehaviour {
 			vx = speed; // 右に進む移動量を入れる
 			leftFlag = false;
 
-            pos = transform.position;
             pos.x += speed;
-            transform.position = pos;
         }
         // もし、左キーが押されたら
         if (Input.GetKey("left") || Input.GetAxis("Horizontal") < 0)
@@ -35,40 +34,24 @@ public class OnKeyPress_MoveGravity : MonoBehaviour {
 			vx = -speed; // 左に進む移動量を入れる
 			leftFlag = true;
 
-            pos = transform.position;
             pos.x += -1 * speed;
-            transform.position = pos;
         }
         // もし、上キーが押されたら
         if (Input.GetKey("up") || Input.GetAxis("Vertical") > 0)
         { 
             vx = speed * 0.4f; // 上に進む移動量を入れる(熱血っぽく奥行きは移動量小)
 
-            pos = transform.position;
             pos.y += vx;
-            transform.position = pos;
         }
         if (Input.GetKey("down") || Input.GetAxis("Vertical") < 0)
         { // もし、下キーが押されたら
             vx = speed * 0.4f; // 下に進む移動量を入れる(熱血っぽく奥行きは移動量小)
 
-            pos = transform.position;
             pos.y += -vx;
-            transform.position = pos;
         }
 
-        //// JOYPAD対応
-        //if (Input.GetAxis("Horizontal") > 0)
-        //{
-        //    leftFlag = false;
-        //}
+        transform.position = pos;
+    }
 
-        //// JOYPAD対応
-        //if (Input.GetAxis("Horizontal") < 0)
-        //{
-        //    leftFlag = true;
-        //}
-	}
-
-	void FixedUpdate() { } // ずっと行う（一定時間ごとに）
+    void FixedUpdate() { } // ずっと行う（一定時間ごとに）
 }
