08efe2a46b6b2f58c6df5775b780c38aeabf6407
github Unibo12 <ryosukeknzw@gmail.com>
2019-07-01 21:27:51 +0900

    [fix]失格になったプレイヤーをカメラ追跡外とする。 また、プレイヤー変数の配列化に併せて修正

--- Assets/Scrips/Forever_ChaseCameraH.cs
+++ Assets/Scrips/Forever_ChaseCameraH.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -22,20 +23,51 @@ public class Forever_ChaseCameraH : MonoBehaviour {
 
     void LateUpdate() { // ずっと行う（いろいろな処理の最後に）
 
-        // カメラは先頭を走るプレイヤーを追いかける
-        if (Nmng.Player[1].NVariable.X < Nmng.Player[0].NVariable.X)
-        {
-            TopPlayerX = Nmng.Player[0].NVariable.X;
-            pos.x = Nmng.Player[0].NVariable.X + Settings.Instance.Game.TopPlayerCameraX;
-        }
-        else
-        {
-            TopPlayerX = Nmng.Player[1].NVariable.X;
-            pos.x = Nmng.Player[1].NVariable.X + Settings.Instance.Game.TopPlayerCameraX;
-        }
+        //何プレイヤーが先頭を走っているか取得
+        int topPlayerNum = TopPlayerNumber(Nmng);
 
-        pos.z = -10; // カメラなので手前に移動させる
+        //カメラは先頭を走るプレイヤーを追いかける
+        TopPlayerX = Nmng.Player[topPlayerNum].NVariable.X;
+        pos.x = Nmng.Player[topPlayerNum].NVariable.X + Settings.Instance.Game.TopPlayerCameraX;
+
+        pos.z = -10; // カメラ位置なのでプレイヤーよりやや手前に移動させる
 		pos.y = base_pos.y; // カメラの元の高さを使う
 		Camera.main.gameObject.transform.position = pos;
 	}
+
+    /// <summary>
+    /// 先頭を走るプレイヤーのプレイヤー番号(1～4)を返す
+    /// </summary>
+    /// <param name="Nmng"></param>
+    /// <returns>TopPlayerNum</returns>
+    int TopPlayerNumber(NekketsuManager Nmng)
+    {
+        //★TODO★
+        //LINQ使えると思うので再度ためす
+        //インデックスを返したい
+
+        float[] playersX;
+        int TopPlayerNum = 0;
+
+        playersX = new float[2];
+
+        playersX[0] = Nmng.Player[0].NVariable.X;
+        playersX[1] = Nmng.Player[1].NVariable.X;
+        //playersX[2] = Nmng.Player[2].NVariable.X;
+        //playersX[3] = Nmng.Player[3].NVariable.X;
+
+        if (playersX[1] < playersX[0]
+            && Nmng.Player[0].NVariable.DeathFlag == DeathPattern.None)
+        {
+            TopPlayerNum = 0;
+        }
+        else if (playersX[0] < playersX[1]
+                && Nmng.Player[1].NVariable.DeathFlag == DeathPattern.None)
+        {
+            TopPlayerNum = 1;
+        }
+
+        return TopPlayerNum;
+    }
+
 }
