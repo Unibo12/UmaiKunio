a150608c5e2d7dbac0ab567ec46fb248633d5115
github Unibo12 <ryosukeknzw@gmail.com>
2019-08-05 23:56:09 +0900

    [work]めりこみたい RectのOverlapsが不要っぽいので削除

--- Assets/Scrips/NekketsuMerikomiCheck.cs
+++ Assets/Scrips/NekketsuMerikomiCheck.cs
@@ -18,11 +18,12 @@ public class NekketsuMerikomiCheck
     {
         // 地面(Y座標)めりこみ補正は着地したタイミングで行う
         // プレイヤーの現在地と障害物が重なっている場合
-        if (NAct.NVariable.Y < NAct.NVariable.mapY)
+
+        if (NAct.NJumpV.squatFlag
+            && NAct.NVariable.Y < NAct.NVariable.mapY)
         {
             NAct.NVariable.Y = NAct.NVariable.mapY; // マイナス値は入れないようにする
         }
-
 
         //テスト用テーブル地形
         //ベタガキなので要修正
@@ -34,57 +35,40 @@ public class NekketsuMerikomiCheck
             && NAct.Nmng.MapObjct1.Box.yMin < NAct.NVariable.Z
             && NAct.NVariable.Z < NAct.Nmng.MapObjct1.Box.yMax)
         {
-            if (NAct.Nmng.MapObjct1.Box.Overlaps(NAct.NAttackV.hurtBox))
+            if (NAct.Nmng.MapObjct1.Box.x - NAct.NVariable.vx < NAct.NVariable.X
+                && NAct.NVariable.X < NAct.Nmng.MapObjct1.Box.x + NAct.NVariable.vx)
             {
-                
-                if (NAct.Nmng.MapObjct1.Box.x - NAct.NVariable.vx < NAct.NVariable.X
-                    && NAct.NVariable.X < NAct.Nmng.MapObjct1.Box.x + NAct.NVariable.vx)
-                {
-                    //障害物の左半分にめり込んでいる場合
-                    NAct.NVariable.X = NAct.Nmng.MapObjct1.Box.x;
-                }
-                if (NAct.Nmng.MapObjct1.Box.x + NAct.Nmng.MapObjct1.Box.width - System.Math.Abs(NAct.NVariable.vx) < NAct.NVariable.X
-                         && NAct.NVariable.X < NAct.Nmng.MapObjct1.Box.x + NAct.Nmng.MapObjct1.Box.width + System.Math.Abs(NAct.NVariable.vx))
-                {
-                    //障害物の右半分にめり込んでいる場合
-                    NAct.NVariable.X = NAct.Nmng.MapObjct1.Box.x + NAct.Nmng.MapObjct1.Box.width;
-                }
+                //障害物の左半分にめり込んでいる場合
+                NAct.NVariable.X = NAct.Nmng.MapObjct1.Box.x;
+            }
+            if (NAct.Nmng.MapObjct1.Box.x + NAct.Nmng.MapObjct1.Box.width - System.Math.Abs(NAct.NVariable.vx) < NAct.NVariable.X
+                     && NAct.NVariable.X < NAct.Nmng.MapObjct1.Box.x + NAct.Nmng.MapObjct1.Box.width + System.Math.Abs(NAct.NVariable.vx))
+            {
+                //障害物の右半分にめり込んでいる場合
+                NAct.NVariable.X = NAct.Nmng.MapObjct1.Box.x + NAct.Nmng.MapObjct1.Box.width;
             }
         }
 
-
-        //// 壁(Z座標)めりこみ補正
-        //if (!NAct.NJumpV.jumpFlag
-        //    && !NAct.NJumpV.squatFlag
-        //    && NAct.NVariable.Y != NAct.Nmng.MapObjct1.topBoxY
-        //    && NAct.Nmng.MapObjct1.TopBox.x - (NAct.Nmng.MapObjct1.myObjectWidth / 2) < NAct.NVariable.X 
-        //    && NAct.NVariable.X < NAct.Nmng.MapObjct1.TopBox.x + (NAct.Nmng.MapObjct1.myObjectWidth / 2))
-        //{
-        //    if (NAct.Nmng.MapObjct1.Box.Overlaps(NAct.NAttackV.hurtBox))
-        //    {
-        //        if (NAct.Nmng.MapObjct1.Box.y < NAct.NVariable.Z
-        //            && NAct.NVariable.Z < NAct.Nmng.MapObjct1.Box.y + (NAct.Nmng.MapObjct1.Box.height / 2))
-        //        {
-        //            //障害物の奥半分にめり込んでいる場合
-        //            NAct.NVariable.Z = NAct.Nmng.MapObjct1.Box.y;
-        //        }
-        //        else if (NAct.Nmng.MapObjct1.Box.y + (NAct.Nmng.MapObjct1.Box.height / 2) < NAct.NVariable.Z
-        //                 && NAct.NVariable.Z < NAct.Nmng.MapObjct1.Box.y + NAct.Nmng.MapObjct1.Box.height)
-        //        {
-        //            //障害物の手前半分にめり込んでいる場合
-        //            NAct.NVariable.Z = NAct.Nmng.MapObjct1.Box.y + NAct.Nmng.MapObjct1.Box.height;
-        //        }
-        //    }
-        //}
-
-
-
-
-
-
-
-
-
+        // 壁(Z座標)めりこみ補正
+        if (!NAct.NJumpV.jumpFlag
+            && !NAct.NJumpV.squatFlag
+            && NAct.NVariable.Y != NAct.Nmng.MapObjct1.topBoxY
+            && NAct.Nmng.MapObjct1.TopBox.x - (NAct.Nmng.MapObjct1.myObjectWidth / 2) < NAct.NVariable.X
+            && NAct.NVariable.X < NAct.Nmng.MapObjct1.TopBox.x + (NAct.Nmng.MapObjct1.myObjectWidth / 2))
+        {
+            if (NAct.Nmng.MapObjct1.Box.y < NAct.NVariable.Z
+                && NAct.NVariable.Z < NAct.Nmng.MapObjct1.Box.y + (NAct.Nmng.MapObjct1.Box.height / 2))
+            {
+                //障害物の奥半分にめり込んでいる場合
+                NAct.NVariable.Z = NAct.Nmng.MapObjct1.Box.y;
+            }
+            else if (NAct.Nmng.MapObjct1.Box.y + (NAct.Nmng.MapObjct1.Box.height / 2) < NAct.NVariable.Z
+                     && NAct.NVariable.Z < NAct.Nmng.MapObjct1.Box.y + NAct.Nmng.MapObjct1.Box.height)
+            {
+                //障害物の手前半分にめり込んでいる場合
+                NAct.NVariable.Z = NAct.Nmng.MapObjct1.Box.y + NAct.Nmng.MapObjct1.Box.height;
+            }
+        }
 
         // ★ここではなく適切な処理場所へ移動すること★
         // 高いところから低いところへ降りた場合
